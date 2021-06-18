f80dc9abfbb602126af26f380b0a452948bf7bac
jpdf00 <j.pdf00@gmail.com>
2021-06-18 19:43:11 -0300

    Add linters

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import Big from 'big.js';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
