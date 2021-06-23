edeadedcb596a4ad4c415b58d4842b63ba39d6f1
jpdf00 <j.pdf00@gmail.com>
2021-06-23 17:43:53 -0300

    Add class for APP
    Bind calculate to buttons.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// Disabling linters on the calculate import because calculate() isn't being used yet.
-/* eslint-disable no-unused-vars */
 import calculate from '../logic/calculate';
-/* eslint-enable no-unused-vars */
 
-const App = () => (
-  <>
-    <Display result="0" />
-    <ButtonPanel />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const data = calculate(this.state, buttonName);
+    this.setState(data);
+  }
+
+  render() {
+    /* eslint-disable no-unused-vars */
+    const { total, next, operation } = this.state;
+    /* eslint-enable no-unused-vars */
+    const result = !next ? total : next;
+
+    return (
+      <>
+        <Display result={result || '0'} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </>
+    );
+  }
+}
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
 
 const Button = (props) => {
-  const { name } = props;
+  const { name, clickHandler } = props;
+  const handleClick = () => (clickHandler(name));
   return (
     <div>
-      <button type="button" name="button">{name}</button>
+      <button type="button" name="button" onClick={handleClick}>{name}</button>
     </div>
   );
 };
 
 Button.propTypes = { name: PropTypes.string.isRequired };
+Button.propTypes = { clickHandler: PropTypes.func.isRequired };
 
 export default Button;
