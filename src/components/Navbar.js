0cbba0ea296cdcfc89bddfce598ebf8789ec041f
jpdf00 <j.pdf00@gmail.com>
2021-06-29 10:40:19 -0300

    Update README.md
    Organize files.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './style.css';
+import '../assets/stylesheets/style.css';
 
 const Navbar = () => (
   <div className="Navbar">
